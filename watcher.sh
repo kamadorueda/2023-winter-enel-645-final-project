54e5fede3d190f17d876adfe1bf6aaf9274c343e
Kevin Amado <kamadorueda@gmail.com>
2023-04-01 01:28:12 -0600

    .

--- watcher.sh
+++ watcher.sh
@@ -1,4 +1,5 @@
 while true; do
+  ro main -X theirs
   scp -i /data/secrets/credentials/holdings-kevin.pem ec2-user@18.116.40.104:/home/ec2-user/ASL_Classification.ipynb .
   scp -i /data/secrets/credentials/holdings-kevin.pem ec2-user@18.116.40.104:/home/ec2-user/training_classifier_asl.h5 training_classifier_asl.h5
   git add .
